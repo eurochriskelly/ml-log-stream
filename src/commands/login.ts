6a8dae4d44db51c771b38d89098af9619f24b695
eurochriskelly@gmail.com <eurochriskelly@gmail.com>
2023-03-05 23:23:47 +0100

    update login interface

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -9,13 +9,59 @@
  */
 
 import { api } from '../lib/api.ts'
+import parseArgs from '../lib/args.ts'
 
 
-console.log('ok here we go')
-const output = await api.v1.eval({
-    src: 'eval/foo.js',
-    database: 'Documents',
-})
+const main = async () => {
+    const args = parseArgs([], ['environment', 'env', 'register', 'help'])
 
-// console.log(outputText);
-console.log(' ok')
+    if (args.errors.length) {
+        console.log(args.error.join('\n'))
+    } else {
+        if (args.switches.help) {
+            showHelp()
+            return
+        }
+
+        // If no environment is specified, try to read from config file
+        if (args.switches.register) {
+            console.log('Registering new environment...')
+            
+            return
+        }
+
+        // Log into the predefined environment
+        if (args.switches.environment) {
+            console.log(`Logging in to MarkLogic...`)
+            const input = 'echo'
+            const output = await api.v1.eval({
+                code: `"${input}"`,
+                format: 'javascript',
+                database: 'Documents',
+            })
+
+            if (output[0] === input) {
+                console.log('Login successful. Should write out connection details to file.')
+            } else {
+                console.log('Login failed. Should write out error details to file.')
+            }
+            return
+        }
+
+        console.log(`No environment specified. Try \`ml login --help\``)
+        console.log(args)
+    }
+}
+
+main()
+
+function showHelp() {
+    console.log(`
+    Usage: ml login [options]
+
+    Options:
+      --environment, -e  The name of the environment to login to
+      --register, -n     Register new environment providing details
+      --help, -h         Show this help message
+    `)
+}
---
+++ src/lib/args.ts
@@ -0,0 +1,122 @@
+/**
+ * Process all command line switches into a single object.
+ *
+ * Known flags are passted in an array called `allowed`.
+ * e.g. allowed = ['help', 'version', 'file']
+ *
+ * Allowed flags are passed to the program preceded by '--' and followed
+ * by a value. e.g. --file=foo.txt or --file foo.txt
+ *
+ * If an ALLOWED_FLAGS item is the only one to start with a particular
+ * character, then it is accepted with that letter. For example, if 'file'
+ * is in ALLOWED_FLAGS then the following are all valid:
+ *   --file=foo.txt
+ *   --file foo.txt
+ *   -f foo.txt
+ *
+ * Args in the form of --arg=value are converted to { arg: value }
+ * Args in the form of --arg value are converted to { arg: value }
+ * Args in the form of --flag are converted to { flag: true }
+ * Args in the form of --flag=false are converted to { flag: false }
+ * Args in the form of --flag=true are converted to { flag: true }
+ *
+ * The result object has the format:
+ * {
+ *  error: [
+ *    string,
+ *    string,
+ *  ],
+ *  switches: {
+ *    switch1: value1,
+ *    switch2: value2,
+ *  }
+ *
+ * If an arg does not exist, it is stored in a property called error.
+ * All other args are stored in a property called switches.
+ *
+ */
+
+interface Result {
+  error: string[]
+  args: any
+}
+
+const parseArgs = (
+  commands: string[] = [],
+  allowed: string[] = [],
+) => {
+  let cmdErr, command = null
+  // check if first arg is a command (e.g. 'login', no '--')
+  {
+    const [first] = Deno.args
+    if (commands.includes(first) && first !== '--') {
+      command = first
+    } else {
+      if (commands.length && first) cmdErr = `Command [${first}] is not a valid command. Must be one of [${commands.join(', ')}]`
+    }
+  }
+  
+  // Gather all args into an array of [key, value] pairs
+  const rest = command ? Deno.args.slice(1) : Deno.args
+  const pairs = rest
+    .map((arg, i) => {
+      if (arg.includes('=')) {
+        return arg.split('=')
+      } else {
+        if (i >= rest.length) {
+          return [arg, 'last']
+        } else {
+          const next = rest[i + 1] || ''
+          if (!next || next.startsWith('--')) {
+            return [arg, true]
+          } else {
+            return [arg, next]
+          }
+        }
+
+      }
+    })
+    .filter(pair => typeof pair[0] == 'string')
+    .filter(x => `${x[0]}`.startsWith('--'))
+
+  // Convert all args to a single object with the format:
+  // {
+  //   command: string,
+  //   error: [
+  //     string,
+  //     string,
+  //   ],
+  //   args: {
+  //     arg1: value1,
+  //     arg2: value2,
+  //   }
+  // }
+  return {
+    command,
+    switches: pairs
+      .filter(x => {
+        const [k, v] = x
+        const name = `${k}`.substring(2)
+        return allowed.length ? allowed.includes(name) : true
+      })
+      .reduce((p: any, n) => {
+        const [k, v] = n
+        const name = `${k}`.substring(2)
+        p[name] = v
+        return p
+      }, {}),
+    errors: [
+      cmdErr, ...pairs
+        .map(p => {
+          const [k, v] = p
+          const name = `${k}`.substring(2)
+          if (allowed.length && !allowed.includes(name)) {
+            return `Flag [${name}] is not allowed. Must be one of [${allowed.join(', ')}]`
+          }
+          return null
+        })
+    ].filter(x => x)
+  }
+}
+
+export default parseArgs
