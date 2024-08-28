68c45942b4e36471d16ed3c55812fa9ecf295296
Chris Kelly <eurochriskelly@gmail.com>
2024-08-29 00:37:17 +0200

    slightly more controlled json generation to support future generation of request log data

--- scripts/jsonify-logs.sh
+++ scripts/jsonify-logs.sh
@@ -9,28 +9,36 @@ else
   DIR=$1
 fi
 
+II() { echo "$(date +%Y-%m-%dT%H:%M:%S%z): <jlog> $@"; }
+
 clear
 # find all log files in the logs directory
-LOG_FILES=$(find "${DIR}" -type f \( \
-  -name "*ErrorLog*txt" \
-  -o -name "*AccessLog*txt" \
-  -o -name "*RequestLog*txt" \))
-# Loop over every file and pass it to a node program
-for file in $LOG_FILES
-do
-  # If the name includes "Error", pass the flag --type error
-  if [[ $file == *"Error"* ]]; then
-    echo "--"
-    echo "Processing ErrorLog $file"
-    node --max-old-space-size=8192 ${LIB_DIR}/src/logFlattenErrorStacks.js --log-file $file --log-file-flat ${file}.tmp
-    node --max-old-space-size=8192 ${LIB_DIR}/src/log2json.js --type error ${file}.tmp
-    rm ${file}.tmp
-  # If the name includes "Access", pass the flag --type access
-  elif [[ $file == *"Access"* ]]; then
-    echo "Processing AccessLog $file"
-    node --max-old-space-size=8192 ${LIB_DIR}/src/log2json.js --type access ${file}
-  else
-    echo "ERROR: File $file does not match any type"
-    exit 1
-  fi
-done 
+
+processLogFiles() {
+  local type=$1
+  LOG_FILES=$(find "${DIR}" -type f -name "*${type}Log*txt")
+  # Loop over every file and pass it to a node program
+  for file in $LOG_FILES
+  do
+    # If the name includes "Error", pass the flag --type error
+    if [[ $file == *"Error"* ]]; then
+      II "--"
+      II "Processing ErrorLog $file"
+      node --max-old-space-size=8192 ${LIB_DIR}/src/logFlattenErrorStacks.js --log-file $file --log-file-flat ${file}.tmp
+      node --max-old-space-size=8192 ${LIB_DIR}/src/log2json.js --type error ${file}.tmp
+      rm ${file}.tmp
+    # If the name includes "Access", pass the flag --type access
+    elif [[ $file == *"Access"* ]]; then
+      II "--"
+      II "Processing AccessLog $file"
+      node --max-old-space-size=8192 ${LIB_DIR}/src/log2json.js --type access ${file}
+    else
+      II "ERROR: File $file does not match any type .. tbd"
+    fi
+    echo "++"
+  done
+}
+
+#processLogFiles "Error"
+processLogFiles "Access"
+# processLogFiles "Request"
