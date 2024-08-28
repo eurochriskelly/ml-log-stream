278c8226d448d4e4077599875d20d4b6ba16874c
Chris Kelly <eurochriskelly@gmail.com>
2024-08-28 23:50:56 +0200

    Cleaning up the easy way
    - If a zip exists, delete everything and use that.
      - It's simpler and safer
    - Fixed error where too many logs were remove in hard path

--- scripts/clean-up.sh
+++ scripts/clean-up.sh
@@ -9,15 +9,37 @@ else
   DIR=$1
 fi
 
+zip=$(find . -name "logs_*.zip" -maxdepth 1)
+ 
 clear
-echo "Clearing temp files ..."
-find "${DIR}" -type f -name "*AccessLog*.txt" -o -name "*ErrorLog*.txt" -exec rm {} \;
+if [ -n "$zip" ]; then
+  # The easy and error free way
+  echo "Removing and re-extracting log files from source zip.."
+  if [ -d logdir ];then rm -rf logdir; fi
+  mkdir -p logdir
+  cp $zip logdir/
+  cd logdir
+  unzip -q $zip
+  rm $zip
+  cd .. 2>/dev/null
+else
+  # more risky approach
+  echo "Clearing temp files ..."
+  find "${DIR}" -type f -name "*.tmp" -exec rm {} \;
 
-echo "Clearing json files ..."
-find "${DIR}" -type f \( \
-  -name "*AccessLog*.json" \
-  -o -name "*ErrorLog*.json" \
-  -o -name "*RequestLog*.json" \) -exec rm {} \;
+  echo "Clearing json files ..."
+  find "${DIR}" -type f \( \
+    -name "*AccessLog*.json" \
+    -o -name "*ErrorLog*.json" \
+    -o -name "*RequestLog*.json" \) -exec rm {} \;
+fi
+
 
 echo "Clearing monster log files ..."
-find "${DIR}" -type f -name "*monster-log*" -o -name "*ErrorLog*.txt" -exec rm {} \;
+find "${DIR}" -type f -name "monster-log*" -exec rm {} \;
+
+if [ -n $(which tree) ];then
+  tree . | head -n 20
+  echo "..."
+fi
+
