f3fe496e6ffce2759ee21aeda5e878bfb520ae8d
Chris Kelly <eurochriskelly@gmail.com>
2024-08-29 00:37:56 +0200

    Improved logging and summary logging

--- scripts/monster-log.sh
+++ scripts/monster-log.sh
@@ -9,6 +9,8 @@ else
   DIR=$1
 fi
 
+II() { echo "$(date +%Y-%m-%dT%H:%M:%S%z): <mlog> $@"; }
+
 clear
 # find all log files in the logs directory
 LOG_FILES=$(find "${DIR}" -type f \( \
@@ -17,12 +19,20 @@ LOG_FILES=$(find "${DIR}" -type f \( \
   -o -name "*RequestLog*.json" \))
 # Loop over every file and pass it to a node program
 mlog=monster-log.csv
+mlogtmp=${mlog}.tmp
 echo "timestamp,date,host,port,type,lineNr,id,source,user,method,url,protocol,statusCode,response,message" > $mlog
+touch $mlogtmp
 for file in $LOG_FILES
 do
   # If the name includes "Error", pass the flag --type error
-  echo "Processing log [$file] with [$(cat $file | wc -l)] entries"
-  node ${LIB_DIR}/src/monsterLog.js --log-file $file --output $mlog
+  II "Processing log [$file] with [$(cat $file | wc -l)] entries"
+  node ${LIB_DIR}/src/monsterLog.js --log-file $file --output $mlogtmp
 done
 
-echo "Total entries in monster log: [$(cat $mlog|wc -l)]"
+II "Total entries in monster log: [$(cat $mlog|wc -l)]"
+II "Sorting monster log..."
+cat $mlogtmp | sort >> $mlog
+rm $mlogtmp
+II "Ready: $mlog"
+
+
