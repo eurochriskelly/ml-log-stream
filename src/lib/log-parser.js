6f76ee08481e10e8053e5ab013407a076eb7e97f
Chris Kelly <eurochriskelly@gmail.com>
2024-08-28 23:51:29 +0200

    Move date processing function into library where it is used

--- src/lib/log-parser.js
+++ src/lib/log-parser.js
@@ -12,10 +12,8 @@ class LogParser {
       .toString()
       .split("\n")
       .map((line, index) => ({ line, lineNr: index }));
-    //.slice(0, 10)
   }
   processLog() {
-    const { mode } = this;
     this.logData = this.logLines
       .filter((x) => x.line.trim())
       .map((x) => {
@@ -112,6 +110,34 @@ class LogParser {
     console.log(`Writing to ${fname}`);
     writeFileSync(fname, this.logData.map((x) => JSON.stringify(x)).join("\n"));
   }
+  exportToCsv(fname) {
+    console.log(`Writing to ${fname}`);
+    writeFileSync(fname, this.logData.map((x) => JSON.stringify(x)).join("\n"));
+  }
 }
 
 module.exports = { LogParser };
+
+function convertDateFormat(dateStr) {
+  // Parse the date string
+  const parts = dateStr.match(
+    /(\d{2})\/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4})/,
+  );
+  if (!parts) return null;
+  const months = {
+    Jan: "01",
+    Feb: "02",
+    Mar: "03",
+    Apr: "04",
+    May: "05",
+    Jun: "06",
+    Jul: "07",
+    Aug: "08",
+    Sep: "09",
+    Oct: "10",
+    Nov: "11",
+    Dec: "12",
+  };
+  const month = months[parts[2]];
+  return `${parts[3]}-${month}-${parts[1]}T${parts[4]}:${parts[5]}:${parts[6]}`;
+}
