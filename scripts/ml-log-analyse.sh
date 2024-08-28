95b919d901a16d50227dd1e5d5bc3e68a795d9d7
Chris Kelly <eurochriskelly@gmail.com>
2024-08-28 21:28:43 +0200

    Supports converting the directory to JSON

--- scripts/ml-log-analyse.sh
+++ scripts/ml-log-analyse.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
+#
+set -e
 
 # When uses runs this command, show them the following options:
 # - logs to json
 # - stream logs
 # - ingest logs
+#
 
-select option in "logs to json" "stream logs" "ingest logs"
-  do
-      case $option in
-          "logs to json")
-              echo "Converting logs to json..."
-              break
-              ;;
-          "stream logs")
-              echo "Streaming logs..."
-              break
-              ;;
-          "ingest logs")
-              echo "Ingesting logs..."
-              break
-              ;;
-          *)
-              echo "Invalid option"
-              ;;
-      esac
-  done
+echo "ml-log-analyse"
+export LIB_DIR="@@REPO_DIR@@"
+
+select option in "logs to json" "stream logs" "ingest logs"; do
+  case $option in
+  "logs to json")
+    echo "Converting logs to json..."
+    bash $LIB_DIR/scripts/jsonify-logs.sh
+    break
+    ;;
+  "stream logs")
+    echo "Streaming logs..."
+    break
+    ;;
+  "ingest logs")
+    echo "Ingesting logs..."
+    break
+    ;;
+  *)
+    echo "Invalid option"
+    ;;
+  esac
+done
